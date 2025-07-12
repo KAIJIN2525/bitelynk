d370c230c0f09ceb05ed0d7d0d74cb75ef97e334
KAIJIN2525 <emememdavid086@gmail.com>
2025-07-12 17:18:33 +0100

    Auth Middleware setup completed and Product model

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,17 @@ const authMiddleware = (req, res, next) => {
     const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
     if (!token) {
-        return res.status(401).json({ success: false, message: "Unauthorized" });
+        return res.status(401).json({ success: false, message: "Unauthorized, no token provided" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {_id:decoded.id, email:decoded.email}; 
+        next();
+    } catch (error) {
+        const message = error.name === "TokenExpiredError" ? "Token expired" : "Invalid token";
+        return res.status(403).json({ success: false, message });
     }
 }
+
+export default authMiddleware;
